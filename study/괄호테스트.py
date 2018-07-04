395873a3da24c6ab844aa6ad3e476aace8b7b62c
jenny.choi <jenny.choi@kakaocorp.com>
2018-07-04 22:42:07 +0900

    괄호테스트

---
+++ study/괄호테스트.py
@@ -0,0 +1,7 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+filtered_1 = [[x for x in row if x % 3 == 0] for row in matrix if sum(row) >= 10]
+filtered_2 = [[x for x in row if (x % 3 == 0)] for row in matrix if sum(row) >= 10]
+filtered_3 = [[x for x in row (if x % 3 == 0)] for row in matrix if sum(row) >= 10]
+
+print(filtered_1)
+print(filtered_2)
