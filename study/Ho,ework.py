d2546e9801d093ebc794d736390dd2ed8b7c25cf
jenny.choi <jenny.choi@kakaocorp.com>
2018-07-15 20:15:25 +0900

    4장 오늘 공부

---
+++ study/Ho,ework.py
@@ -0,0 +1,18 @@
+class Homework(object):
+    def __init__(self):
+        self._grade = 0
+
+    @property
+    def grade(self):
+        return self._grade
+
+    @grade.setter
+    def grade(self, value):
+        if not (0 <= value <= 100):
+            raise ValueError('Grade must be between 0 and 100')
+        self._grade = value
+
+
+galieo = Homework()
+galieo.grade = 95
+
